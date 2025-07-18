5ef554636476ceb1320060dd2e44f9f50189a21f
shefket-mustafa <miracle_1@abv.bg>
2025-07-18 22:27:19 +0300

    Header made responsive with a burger menu

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,33 +1,54 @@
-import {Link} from 'react-router'
-import {motion} from "motion/react"
+        import {Link} from 'react-router'
+        import {motion} from "motion/react"
+        import { useState } from 'react';
+        import { IoClose, IoMenu } from 'react-icons/io5';
 
-export default function Header(){
+        export default function Header(){
 
-    return (
+            const [isOpen, setIsOpen] = useState(false);
 
-        <motion.div
-        initial={{ opacity: 0, y: -100 }}
-        animate={{ opacity: 1, y: 0 }}
-       whileInView={{ opacity: 1, y: 0 }}
-       transition={{ duration: 0.3, ease: "easeOut" }}
-       viewport={{ once: true }}
-        className='flex sticky top-0 z-30 justify-between items-center px-15 h-20 bg-black/90 text-white'>
+            const toggleMenu = () => setIsOpen(prev => !prev);
 
-        <div className='flex text-3xl'>
-            <Link to='/'>casaFind</Link>
-        </div>
+            return (
 
-        <div className='flex text-lg gap-10'>
-            <Link to='/catalog'>Find a Home</Link>
-            <Link to='/sell'>Sell</Link>
-            <Link to='contact'>Contact</Link>
-            <Link to='about'>About us</Link>
-        </div>
+                <motion.div
+                initial={{ opacity: 0, y: -100 }}
+                animate={{ opacity: 1, y: 0 }}
+            whileInView={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.3, ease: "easeOut" }}
+            viewport={{ once: true }}
+                className='sticky top-0 z-30 justify-between items-center px-5 h-20 bg-black/90 text-white'>
 
-        <div>
-            {/* <p>Guest</p> */}
-        </div>
+                <div className='flex justify-between items-center px-6 h-20'>
 
-        </motion.div>
-    )
-}
+                {/* Logo */}
+                <div className='flex text-3xl'>
+                    <Link to='/'>casaFind</Link>
+                </div>
+
+                {/* Desktop menu */}
+                <nav className='hidden md:flex text-lg gap-10'>
+                    <Link to='/catalog'>Find a Home</Link>
+                    <Link to='/sell'>Sell</Link>
+                    <Link to='contact'>Contact</Link>
+                    <Link to='about'>About us</Link>
+                </nav>
+
+                {/* Burger icon */}
+                <div className='md:hidden text-3xl cursor-pointer' onClick={toggleMenu}>
+                    {isOpen ? <IoClose/> : <IoMenu/>}
+                </div>
+                </div>
+
+                {/* Mobile burger Menu */}
+                {isOpen && (
+                    <div className='md:hidden bg-black/95 text-white flex flex-col gap-6 px-6 pb-6'>
+                    <Link to='/catalog' onClick={toggleMenu}>Find a Home</Link>
+                        <Link to='/sell' onClick={toggleMenu}>Sell</Link>
+                        <Link to='contact' onClick={toggleMenu}>Contact</Link>
+                        <Link to='about' onClick={toggleMenu}>About us</Link>
+                    </div>
+                )}
+                </motion.div>
+            )
+        }
