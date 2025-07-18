03e96feab99df7b7d49620c8f2ba8259d958a056
shefket-mustafa <miracle_1@abv.bg>
2025-07-18 21:59:05 +0300

    framer motion removed from Find Home component as it was causing an issue

--- src/pages/FindHome.tsx
+++ src/pages/FindHome.tsx
@@ -43,11 +43,7 @@ export default function FindHome() {
   return (
     <div className="w-full bg-white">
       {/* Hero */}
-      <motion.div
-        initial={{ opacity: 0, y: 30 }}
-        animate={{ opacity: 1, y: 0 }}
-        transition={{ duration: 0.8, ease: "easeOut" }}
-        viewport={{once:true}}
+      <div
         className="relative h-[450px]"
       >
         <div className="absolute inset-0 z-0 bg-[url('https://robbreport.com/wp-content/uploads/2023/02/41Popcorn_HiRes_23080901-1.jpg?w=800')] bg-cover" />
@@ -62,7 +58,7 @@ export default function FindHome() {
         </div>
         <div className="absolute bottom-0 left-0 w-1/2 h-[80px] bg-white/50 z-10 clip-x-slant"></div>
         <div className="absolute bottom-0 right-0 w-1/2 h-[80px] bg-white/50 z-10 clip-x-slant-reverse"></div>
-      </motion.div>
+      </div>
 
       {/* Controls */}
       <motion.div
