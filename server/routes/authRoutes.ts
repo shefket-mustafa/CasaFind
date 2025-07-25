98929c0e7ed7c329c09ba958f3a85854a78835d5
shefket-mustafa <miracle_1@abv.bg>
2025-07-25 14:14:47 +0300

    Login logic added

--- server/routes/authRoutes.ts
+++ server/routes/authRoutes.ts
@@ -14,7 +14,7 @@ if (!JWT_SECRET) {
   authRoutes.post('/register', async (req,res) => {
     const {email, password} = req.body;
     try{
-        const existing = await User.findOne(email);
+        const existing = await User.findOne({email});
         if(existing){
             return res.status(400).json({message: 'Email already in use!'})
         }
@@ -41,7 +41,6 @@ authRoutes.post('/login', async (req,res) => {
         if(!user) {
             return res.status(400).json({message: 'Invalid credentials!'})
         }
-
         const match = await bcrypt.compare(password, user.hashedPassword);
         if(!match) {
             return res.status(400).json({message: 'Invalid credentials!'})
