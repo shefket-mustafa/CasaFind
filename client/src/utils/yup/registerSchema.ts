820f42f87e4d52da56b6102404a5bda57f317b08
shefket-mustafa <shefket.must@gmail.com>
2026-05-28 11:31:08 +0300

    refactor: remove complicated password validation

--- client/src/utils/yup/registerSchema.ts
+++ client/src/utils/yup/registerSchema.ts
@@ -1,13 +1,7 @@
-
 import * as yup from "yup";
 
 export const registerSchema = yup.object().shape({
-  email: yup
-    .string()
-    .email("Invalid email!")
-    .min(3, "Email must be at least 3 characters")
-    .max(50, "Email is too long")
-    .required("Email is required!"),
+  email: yup.string().email("Invalid email!").required("Email is required!"),
   password: yup
     .string()
     .min(6, "Password must be at least 6 characters")
